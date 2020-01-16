912dada728f47354860a6f4a43e734ef08b3e6d8
omofish <jason_lzx@hotmail.com>
2020-01-16 00:48:43 -0600

    added card grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import 'semantic-ui-css/semantic.min.css'
+import { Header, Card } from "semantic-ui-react";
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
+      const response = await fetch("./data/products.json");
       const json = await response.json();
       setData(json);
     };
@@ -14,10 +16,23 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <React.Fragment>
+      <Card.Group>
+        {products.map(product => (
+          <Product key={product.sku} product={product} />
+        ))}
+      </Card.Group>
+    </React.Fragment>
   );
 };
+
+const Product = ({ product }) => (
+  <Card>
+    <Card.Content>
+      <Card.Header>{product.title}</Card.Header>
+      <Card.Description>{product.sku}</Card.Description>
+    </Card.Content>
+  </Card>
+);
 
 export default App;
