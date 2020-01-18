2e128be4f31de9378c604bb1c3c476524c996c0b
omofish <jason_lzx@hotmail.com>
2020-01-18 10:32:40 -0600

    reformatting and added shopping cart button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Card, Image, Header, Divider, Button } from "semantic-ui-react";
+import {
+  Card,
+  Image,
+  Header,
+  Sidebar,
+  Button,
+  Menu,
+  Segment,
+  Grid,
+  Icon,
+  GridColumn,
+  Container
+} from "semantic-ui-react";
 import { to2DP } from "./components/utils";
 
 const App = () => {
@@ -18,14 +30,26 @@ const App = () => {
 
   return (
     <React.Fragment>
-      <Card.Group>
-        {products.map(product => (
-          <Product key={product.sku} product={product} />
-        ))}
-      </Card.Group>
+    <Menu fixed="right" vertical compact>
+      <Menu.Item>
+      <CartButton/></Menu.Item>
+    </Menu>
+      <Container>
+        <Segment>
+          <Catalog products={products} />
+        </Segment>
+      </Container>
     </React.Fragment>
   );
 };
+
+const Catalog = ({ products }) => (
+  <Card.Group centered itemsPerRow="4" stackable>
+    {products.map(product => (
+      <Product key={product.sku} product={product} />
+    ))}
+  </Card.Group>
+);
 
 const Product = ({ product }) => {
   const imageURL = "data/products/" + product.sku + "_2.jpg";
@@ -33,14 +57,13 @@ const Product = ({ product }) => {
 
   return (
     <Card>
-      <Image src={imageURL} wrapped ui={false}/>
+      <Image src={imageURL} wrapped ui={false} />
       <Card.Content>
         <Card.Header textAlign="center">{product.title}</Card.Header>
         <Card.Meta textAlign="center">{product.description}</Card.Meta>
-        <Card.Content>
-          <Divider />
-          <Header textAlign="center">{price}</Header>
-        </Card.Content>
+      </Card.Content>
+      <Card.Content extra>
+        <Header textAlign="center">{price}</Header>
         <SizeButtons />
       </Card.Content>
     </Card>
@@ -55,12 +78,19 @@ const sizes = {
 };
 
 const SizeButtons = () => (
-  <Button.Group fluid widths="10">
-    <Button>S</Button>
-    <Button>M</Button>
-    <Button>L</Button>
-    <Button>XL</Button>
+  <Button.Group fluid>
+    {Object.keys(sizes).map(size => (
+      <Button>{size}</Button>
+    ))}
   </Button.Group>
-)
+);
+
+const CartButton = () => <Button icon="shopping cart" />;
+
+const ShoppingCart = () => {
+  const [visible, setVisible] = { name: "visible" };
+
+  return <Sidebar></Sidebar>;
+};
 
 export default App;
